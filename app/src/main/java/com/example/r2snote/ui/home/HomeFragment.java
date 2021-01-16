00e66127da5c1d9a0fc0ca748415fda5e7e75e09
hoanghui <hoanghuy159@gmail.com>
2021-01-16 16:09:23 +0700

    Update login, register

--- app/src/main/java/com/example/r2snote/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/r2snote/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.r2snote.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.example.r2snote.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    AnyChartView anyChartView;
+
+    String[] months = {"Jan", "Feb", "Mar"};
+    int[] earnings = {500,800,2000};
 
     private HomeViewModel homeViewModel;
 
@@ -30,6 +44,25 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Log.d("huy", "aloooooooooooooooooooooooooooooooooo");
+        //anyChartView = anyChartView.findViewById(R.id.any_chart_view);
+
+        //setupPieChart();
+
         return root;
     }
+
+//    public void setupPieChart(){
+//        Pie pie = AnyChart.pie();
+//
+//        List<DataEntry> dataEntries = new ArrayList<>();
+//
+//        for(int i = 0; i < months.length; i++){
+//            dataEntries.add(new ValueDataEntry(months[i], earnings[i]));
+//        }
+//
+//        pie.data(dataEntries);
+//        anyChartView.setChart(pie);
+//    }
 }
